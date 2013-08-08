05c300b98ac14e7e48a60b19b5a9ebbfb3af894d
jianping.xwh <jianping.xwh@taobao.com>
2013-08-08 15:20:02 +0800

    add uploader theme

--- 1.4/build/plugin/ui/uploader.js
+++ 1.4/build/plugin/ui/uploader.js
@@ -290,6 +290,6 @@ KISSY.add('gallery/butterfly/1.4/plugin/ui/uploader',function(S, Node, Base, Upl
   });
   return UploaderUi;
 }, {
-  requires: ['node', './base', 'gallery/uploader/1.4/', 'gallery/uploader/1.4/plugins/auth/auth', 'gallery/uploader/1.4/plugins/urlsInput/urlsInput', 'gallery/uploader/1.4/plugins/proBars/proBars', 'gallery/uploader/1.4/plugins/tagConfig/tagConfig', 'gallery/uploader/1.4/themes/imageUploader/index', 'gallery/uploader/1.4/themes/imageUploader/style.css']
+  requires: ['node', './base', 'gallery/uploader/1.4/', 'gallery/uploader/1.4/plugins/auth/auth', 'gallery/uploader/1.4/plugins/urlsInput/urlsInput', 'gallery/uploader/1.4/plugins/proBars/proBars', 'gallery/uploader/1.4/plugins/tagConfig/tagConfig', 'gallery/uploader/1.4/themes/grayUploader/index', 'gallery/uploader/1.4/themes/grayUploader/style.css']
 });
 
