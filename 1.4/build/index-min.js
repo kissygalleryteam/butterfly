06c63452bb50f7d7e0838a316ed3bcce21614b08
jianping.xwh <jianping.xwh@taobao.com>
2013-09-03 10:00:23 +0800

    fix bug

--- 1.4/build/index-min.js
+++ 1.4/build/index-min.js
@@ -1,3 +1,3 @@
-/*! butterfly - v1.4 - 2013-09-02 9:28:24 PM
+/*! butterfly - v1.4 - 2013-09-03 10:00:04 AM
 * Copyright (c) 2013 明河; Licensed  */
 KISSY.add("gallery/butterfly/1.4/index",function(a,b,c){var d,e;return d=b.all,e=c.extend({constructor:function(a,b){var c;return null==b&&(b={}),c=this,e.superclass.constructor.call(c,b),c.set("target",a)},render:function(){return this._eachEl()},field:function(){},_eachEl:function(){var b,c,d,e,f,g;return g=this,d=g.get("target"),d.length?(f=d.getDOMNode().elements,a.each(f,function(){}),b=d.all("input"),b.each(function(a){return g.fire("inputEach",{$el:a})}),c=d.all("select"),c.each(function(a){return a.attr("type","select"),g.fire("selectEach",{$el:a})}),e=d.all("textarea"),e.each(function(a){return a.attr("type")||a.attr("type","textarea"),g.fire("textareaEach",{$el:a})})):!1}},{ATTRS:{target:{value:"",getter:function(a){return d(a)}},uis:{value:{}}}})},{requires:["node","rich-base"]});
--- 1.4/build/plugin/bidi-min.js
+++ 1.4/build/plugin/bidi-min.js
@@ -1,3 +1,3 @@
-/*! butterfly - v1.4 - 2013-09-02 9:28:24 PM
+/*! butterfly - v1.4 - 2013-09-03 10:00:04 AM
 * Copyright (c) 2013 明河; Licensed  */
 KISSY.add("gallery/butterfly/1.4/plugin/bidi",function(a,b,c){var d,e,f;return d=b.all,f=function(a,b){var c;return a&&a.length?(c=a.getDOMNode().tagName,c===b):!1},e=function(a){return e.superclass.constructor.call(this,a),this.set("config",a)},a.extend(e,c,{pluginInitializer:function(a){var b,c;return this.set("host",a),(b=this.get("bidi"))?(c=b.model,c.on("render:linkage",function(a){var b,c;return b=a.el,f(b,"SELECT")?(c=b.data("data-ui"),c.sync&&c.sync()):void 0})):!1}},{ATTRS:{pluginId:{value:"bidi"},bidi:{value:""},target:{value:"",getter:function(a){return d(a)}},host:{value:""}}}),e},{requires:["node","base"]});
