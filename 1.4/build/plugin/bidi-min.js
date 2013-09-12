1e93a10b32bcbaf88d628897cbaf3463753dbb82
jianping.xwh <jianping.xwh@taobao.com>
2013-09-12 17:00:33 +0800

    fix bug

--- 1.4/build/plugin/bidi-min.js
+++ 1.4/build/plugin/bidi-min.js
@@ -1,3 +1,3 @@
-/*! butterfly - v1.4 - 2013-09-11 8:50:08 PM
+/*! butterfly - v1.4 - 2013-09-12 4:57:37 PM
 * Copyright (c) 2013 明河; Licensed  */
 KISSY.add("gallery/butterfly/1.4/plugin/bidi",function(a,b,c){var d,e,f;return d=b.all,f=function(a,b){var c;return a&&a.length?(c=a.getDOMNode().tagName,c===b):!1},e=function(a){return e.superclass.constructor.call(this,a),this.set("config",a)},a.extend(e,c,{pluginInitializer:function(b){var c,d;return this.set("host",b),(c=this.get("bidi"))?(d=c.model,d.on("render:linkage",function(b){var c,d;return c=b.el,f(c,"SELECT")&&(d=c.data("data-ui"),d&&a.isFunction(d.sync))?d.sync():void 0})):!1}},{ATTRS:{pluginId:{value:"bidi"},bidi:{value:""},target:{value:"",getter:function(a){return d(a)}},host:{value:""}}}),e},{requires:["node","base"]});
